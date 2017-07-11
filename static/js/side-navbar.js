2b4497ba64fd97367a73e3dcdae80751d5ed84dc
YoungSoo Kim <znxkznxk1030@gmail.com>
2017-07-12 01:24:00 +0900

    side bar

--- static/js/side-navbar.js
+++ static/js/side-navbar.js
@@ -1,7 +1,17 @@
 function openSideBar(){
     document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeSideBar(){
     document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
+
+btn=document.querySelector('.logo-menu');
+btn.addEventListener('click', function(){
+        $('.sidenav').toggleClass('open-nav');
+        $('#main').toggleClass('open');
+    }
+);
+

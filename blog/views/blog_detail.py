d767a7c3a11879bb70c0881231f2a1df2c961c25
ys <znxkznxk1030@gmail.com>
2017-08-18 03:54:47 +0900

    + MarkDown & CodeHighter pip install mistune pip install pygments
    usage
    ---------- markdownify.py ----------
    register = template.Library()
    
    @register.filter
    def markdown(value)
    ...
    return markdown(value)
    
    ---------- detail.html ----------
    
    {% load markdownify %}
    
    {{ post.content | markdown | safe }}
    
    -------- + ------
    
    + native.css

--- blog/views/blog_detail.py
+++ blog/views/blog_detail.py
@@ -4,14 +4,21 @@ from django.shortcuts import render
 from category.models import Category
 from blog.models import Posts
 
+import markdown
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
 class BlogDetailView(View):
     def get(self, request, *args, **kwargs):
-        print(Posts.objects.filter(slug=kwargs['blog_slug']))        
+        post = Posts.objects.get(slug=kwargs['blog_slug'])
+        print(post)
         return render(
                 request,
                 'blog/detail.html',
                 context={
                     'categories': Category.objects.all(),
-                    'post':Posts.objects.get(slug=kwargs['blog_slug']),
+                    'post': post,
                     }
                 )

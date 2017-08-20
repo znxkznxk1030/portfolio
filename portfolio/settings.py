37a5bf2baddc8ac5e1863a63d15b96bf766c45e0
YoungSoo Kim <znxkznxk1030@gmail.com>
2017-08-21 04:42:23 +0900

    add media root on setting

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT_DIR = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,3 +124,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
             os.path.join(BASE_DIR, 'static'),
         )
+
+MEDIA_ROOT = os.path.join(
+        PROJECT_ROOT_PATH,
+        "dist",
+        "media",
+        )
+
+MEDIA_URL = '/media/'
